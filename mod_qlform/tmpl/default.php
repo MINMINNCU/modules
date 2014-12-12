fb4a48d8d4cdd464a55ba0e343cc3c58233edb18
Rosalie Chang <rosalie811030@gmail.com>
2014-12-13 02:36:54 +0800

    quotation css

--- mod_qlform/tmpl/default.php
+++ mod_qlform/tmpl/default.php
@@ -100,17 +100,19 @@ $register=$obj_helper->getRegister();
         $url='/develop/media/k2/users/'.$user[0]->image;
 
         ?>
-        <div class="quotation" style="width: auto;">
+        <div style="float:left; height:auto;  ">
+        <img src="<?php echo JURI::base(true); ?>/media/k2/users/<?php echo $user[0]->image; ?>" alt="<?php echo $user->userName; ?>" width="60px" style="margin-top:14px; margin-left:4px; border-radius: 50%; border:1px solid #ccc;"/>
+        </div>
+        
+        <div class="quotation" style="margin-left:85px; width: auto;">
 
           <input class='q_obj' style="display: none;" userId='<?php echo $user_id; ?>' price='<?php echo $quotations[$i]->quotation; ?>'>
-          <!-- <img src="<?php echo $url; ?>" alt="<?php echo $user->userName; ?>" width="48" class="userimg"/> <?php echo $quotations[$i]->created; ?> -->
-          <div style="float:left; height:auto;"><img src="/minmin/templates/mynewtemplate/images/level2.png" alt="<?php echo $user->userName; ?>" width="48" class="userimg"/></div>
-
           <div>
             <div style="height:48px; line-height:48px; font-size:17px; float:left; margin-left:12px">報價 “</div>
             <div style="height:48px; line-height:48px; font-size:26px;"><?php echo " ".$quotations[$i]->quotation." ”";?></div>
-            <div style="font-size:17px; margin-left:18px;"><?php echo $quotations[$i]->description; ?></div>
+            <div style="font-size:17px; margin-left:13px;"><?php echo $quotations[$i]->description; ?></div>
           </div>
+          
           <p align="right"><?php echo $quotations[$i]->created; ?></p>
 
           <?php if($register->id==$author): ?>
